2036495381853e32a07bd4bd2e144410911140f1
RaghavAwasthi <raghavawasthi2014@gmail.com>
2018-02-23 20:17:34 +0530

    Updated little code

--- app/src/main/java/com/rvai/musicplayer/MainActivity.java
+++ app/src/main/java/com/rvai/musicplayer/MainActivity.java
@@ -1,7 +1,10 @@
 package com.rvai.musicplayer;
 
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final MediaPlayer mediaPlayer = MediaPlayer.create(this,R.raw.song);
+
+
+        Button play=(Button)findViewById(R.id.play);
+        play.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mediaPlayer.start();
+
+            }
+        });
+
+        Button pause=(Button)findViewById(R.id.pause);
+        pause.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+mediaPlayer.pause();
+            }
+        });
     }
 }
